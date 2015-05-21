ceafaf432f981244247089589f3f2beb547a8e01
Nicolastinkl <nicolastinkl@gmail.com>
2015-05-21 21:16:03 +0800

    change

--- AI2020OS/View Controllers/AIMessageCenterViewController.swift
+++ AI2020OS/View Controllers/AIMessageCenterViewController.swift
@@ -13,5 +13,23 @@ class AIMessageCenterViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        var oneButton = UIButton.buttonWithType(.Custom)
+        oneButton.setImage(UIImage(named: "ico_user"), forState: UIControlState.Normal)
+        let userItem =  UIBarButtonItem(customView: oneButton as UIView)
+        
+        
+        var twoButton = UIButton.buttonWithType(.Custom)
+        twoButton.setImage(UIImage(named: "ico_more"), forState: UIControlState.Normal)
+        let twoItem =  UIBarButtonItem(customView: twoButton as UIView)
+        
+        
+        
+        navigationItem.rightBarButtonItems = [userItem,twoItem]
+        
+    }
+    
+    @IBAction func backViewAction(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
     }
 }
