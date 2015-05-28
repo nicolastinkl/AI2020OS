bc9444e6d82b7c0cbab98e7fd7cca02a0a411c29
liliang <liliang9@asiainfo.com>
2015-05-28 17:03:22 +0800

    修改服务类列表的cell

--- AI2020OS/View Controllers/AIServiceCatalogViewController.swift
+++ AI2020OS/View Controllers/AIServiceCatalogViewController.swift
@@ -10,10 +10,25 @@ import UIKit
 
 class AIServiceCatalogViewController: UIViewController {
 
+    
+    @IBOutlet weak var catalogTable: UITableView!
+    @IBOutlet weak var serviceTable: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
 
-        // Do any additional setup after loading the view.
+        let nib = UINib(nibName: "AICatalogTableCellTableViewCell", bundle: nil)
+        catalogTable.registerNib(nib,
+            forCellReuseIdentifier: "CatalogCell")
+        catalogTable.separatorInset = UIEdgeInsetsZero
+        catalogTable.layoutMargins = UIEdgeInsetsZero
+        catalogTable.dataSource = self
+        catalogTable.delegate = self
+        
+        serviceTable.dataSource = self
+        serviceTable.delegate = self
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -33,3 +48,55 @@ class AIServiceCatalogViewController: UIViewController {
     */
 
 }
+
+extension AIServiceCatalogViewController : UITableViewDelegate, UITableViewDataSource {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if tableView == catalogTable {
+            return 5
+        } else if tableView == serviceTable {
+            return 10
+        }
+        
+        return 0
+    }
+    
+    // Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
+    // Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
+    
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return CGFloat(60)
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+
+    //    var cell  = tableView.dequeueReusableCellWithIdentifier(myIdentifier) as UITableViewCell
+        
+        var cell: UITableViewCell!
+        if tableView == catalogTable {
+            let catalogCell  = tableView.dequeueReusableCellWithIdentifier("CatalogCell") as AICatalogTableCellTableViewCell
+            catalogCell.name = "catalog"
+            cell = catalogCell
+        } else if tableView == serviceTable {
+            cell  = tableView.dequeueReusableCellWithIdentifier("ServiceCell") as UITableViewCell
+            cell!.textLabel.text = "service"
+        }
+        
+        cell.setHeight(CGFloat(60))
+        return cell!
+    }
+    
+    func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+
+        catalogTable.respondsToSelector(aSelector: Selector)
+        catalogTable.separatorInset = UIEdgeInsetsZero
+        catalogTable.layoutMargins = UIEdgeInsetsZero
+    }
+    
+}
+
+ 
