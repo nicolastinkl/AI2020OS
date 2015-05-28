6d37a352429ce2c7c4ce530425aa17ca02eb23d3
wantsor <liuxian_2004@sina.com>
2015-05-28 18:00:40 +0800

    #add# OrderListModel
    #add# OrderListModel

---
+++ AI2020OS/Services/AIOrderService.swift
@@ -0,0 +1,18 @@
+//
+//  AIOrderService.swift
+//  AI2020OS
+//
+//  Created by 刘先 on 15/5/28.
+//  Copyright (c) 2015年 ___ASIAINFO___. All rights reserved.
+//
+
+import Foundation
+import JSONJoy
+
+class AIOrderRequester {
+    typealias OrderListRequesterCompletion = (data:[AIOrderListModel]) ->()
+    
+    func queryOrderList(page :Int=1,completion:OrderListRequesterCompletion){
+        
+    }
+}
