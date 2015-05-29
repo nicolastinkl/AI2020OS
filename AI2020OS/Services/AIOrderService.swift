92873fdb30d7bd250594ca6fe572b006ba966f35
liuxian <liuxian@google.com>
2015-05-29 09:41:07 +0800

    #add# orderservice

--- AI2020OS/Services/AIOrderService.swift
+++ AI2020OS/Services/AIOrderService.swift
@@ -11,8 +11,26 @@ import JSONJoy
 
 class AIOrderRequester {
     typealias OrderListRequesterCompletion = (data:[AIOrderListModel]) ->()
+    private var isLoading : Bool = false
     
+    //查询订单列表 
     func queryOrderList(page :Int=1,completion:OrderListRequesterCompletion){
         
+        if isLoading {
+            return
+        }
+        isLoading = true
+        
+        AIHttpEngine.postRequestWithParameters(AIHttpEngine.ResourcePath.GetOrderList, parameters: ["page_num":"1","order_state":"1"]) {  [weak self] (response, error) -> () in
+            if let strongSelf = self{
+                strongSelf.isLoading = false
+            }
+            if let responseJSON: AnyObject = response{
+                let orders =  AIOrderListModel(JSONDecoder(responseJSON))
+                completion(data: orders.orderArray)
+            }else{
+                completion(data: [])
+            }
+        }
     }
 }
