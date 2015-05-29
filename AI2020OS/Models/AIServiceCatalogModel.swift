d75d1907d3244112058e3992a579e68bbf9e8c07
liliang <liliang9@asiainfo.com>
2015-05-29 16:50:46 +0800

    修改服务目录模型

--- AI2020OS/Models/AIServiceCatalogModel.swift
+++ AI2020OS/Models/AIServiceCatalogModel.swift
@@ -7,3 +7,33 @@
 //
 
 import Foundation
+import JSONJoy
+
+//订单列表model
+struct AICatalogListModel : JSONJoy {
+    var catalogArray: Array<AICatalogItemModel>?
+    init(_ decoder: JSONDecoder) {
+        if let addrs = decoder.array {
+            catalogArray = Array<AICatalogItemModel>()
+            for item in addrs {
+                catalogArray?.append(AICatalogItemModel(item))
+            }
+        }
+    }
+}
+
+//订单列表项model
+struct AICatalogItemModel : JSONJoy{
+
+    var catalog_id : Int?
+    var catalog_name : String?
+    
+    init(){
+        
+    }
+    
+    init(_ decoder: JSONDecoder) {
+        catalog_id = decoder["order_id"].integer
+        catalog_name = decoder["order_number"].string
+    }
+}
