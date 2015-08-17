4ee2242aca35c53830597dfc3dfcbd05312d1153
wangli <wangli@wanglideMacBook-Pro.local>
2015-08-17 20:49:42 +0800

    tongbu

---
+++ AI2020OS/View Controllers/Message/AIMessageModel.h
@@ -0,0 +1,13 @@
+//
+//  AIMessageModel.h
+//  AI2020OS
+//
+//  Created by 王坜 on 15/8/17.
+//  Copyright (c) 2015年 ___ASIAINFO___. All rights reserved.
+//
+
+#import "JSONModel.h"
+
+@interface AIMessageModel : JSONModel
+
+@end
---
+++ AI2020OS/View Controllers/Message/AIMessageModel.m
@@ -0,0 +1,13 @@
+//
+//  AIMessageModel.m
+//  AI2020OS
+//
+//  Created by 王坜 on 15/8/17.
+//  Copyright (c) 2015年 ___ASIAINFO___. All rights reserved.
+//
+
+#import "AIMessageModel.h"
+
+@implementation AIMessageModel
+
+@end
