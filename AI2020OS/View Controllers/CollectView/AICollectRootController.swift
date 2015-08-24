57f0d7ef4dd20851296152133398381d008cfc28
tinkl <nicolastinkl@gmail.com>
2015-08-24 21:12:20 +0800

    ＃ 添加收藏夹

---
+++ AI2020OS/View Controllers/CollectView/AICollectRootController.swift
@@ -0,0 +1,14 @@
+//
+//  AICollectRootController.swift
+//  AI2020OS
+//
+//  Created by admin on 8/24/15.
+//  Copyright (c) 2015 ___ASIAINFO___. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AICollectRootController: UIViewController{
+
+}
