161cd2cc761f143e319dfa200b17f39af361665d
tinkl <nicolastinkl@gmail.com>
2015-08-25 11:24:28 +0800

    # 收藏夹 移植 AITrans项目里的解析收藏处理

--- AI2020OS/View Controllers/CollectView/AICollectRootController.swift
+++ AI2020OS/View Controllers/CollectView/AICollectRootController.swift
@@ -10,5 +10,19 @@ import Foundation
 import UIKit
 
 class AICollectRootController: UIViewController{
+    @IBOutlet weak var containView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
+        self.title = "收藏夹"
+        
+        let viewController = UIStoryboard(name: AIApplication.MainStoryboard.MainStoryboardIdentifiers.UICollectMoreStoryboard, bundle: nil).instantiateInitialViewController() as UIViewController
+        
+        //self.containView.addSubview(viewController.view)
+        self.containView.hidden = true
+        self.addChildViewController(viewController)
+        self.view.addSubview(viewController.view)
+        
+    }
 }
