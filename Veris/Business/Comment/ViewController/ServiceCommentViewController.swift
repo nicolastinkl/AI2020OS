305a6c77a34ac576e732f85d2cd3f5c2cb017aab
admin <liliang9@asiainfo.com>
2016-06-15 15:18:28 +0800

    add Chinese string

--- Veris/Business/Comment/ViewController/ServiceCommentViewController.swift
+++ Veris/Business/Comment/ViewController/ServiceCommentViewController.swift
@@ -16,6 +16,11 @@ class ServiceCommentViewController: UIViewController {
     @IBOutlet weak var starsDes: UILabel!
     @IBOutlet weak var commentDistrict: CommentDistrictView!
     
+    class func loadFromXib() -> ServiceCommentViewController {
+        let vc = ServiceCommentViewController(nibName: "ServiceCommentViewController", bundle: nil)
+        return vc
+    }
+    
     var starRateView: CWStarRateView!
     override func viewDidLoad() {
         super.viewDidLoad()
