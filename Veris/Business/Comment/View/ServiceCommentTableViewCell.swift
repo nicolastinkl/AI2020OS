f59ae1112ce59228adeb7ee5ff647dca3606128a
admin <liliang9@asiainfo.com>
2016-06-16 14:57:00 +0800

    修改星级评价View

--- Veris/Business/Comment/View/ServiceCommentTableViewCell.swift
+++ Veris/Business/Comment/View/ServiceCommentTableViewCell.swift
@@ -10,12 +10,29 @@ import UIKit
 
 class ServiceCommentTableViewCell: UITableViewCell {
 
-    var starRateView: CWStarRateView!
+    @IBOutlet weak var serviceIcon: UIImageView!
     @IBOutlet weak var starsContainerView: UIView!
+    
+    @IBOutlet weak var heightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var starContainerWidthConstraint: NSLayoutConstraint!
+    @IBOutlet weak var starContainerHeightConstraint: NSLayoutConstraint!
+    var starRateView: StarRateView!
+    
+    private var originIconHeight: CGFloat!
+    private var originStarContainerHeight: CGFloat!
+    private var originStarContainerWidth: CGFloat!
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        
+        serviceIcon.clipsToBounds = true
+        
+        serviceIcon.image = UIImage(named: "testHolder1")
+        
+        originIconHeight = heightConstraint.constant
+        originStarContainerHeight = starContainerHeightConstraint.constant
+        originStarContainerWidth = starContainerWidthConstraint.constant
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
@@ -26,14 +43,41 @@ class ServiceCommentTableViewCell: UITableViewCell {
     
     override func layoutSubviews() {
         if starRateView == nil {
-            starRateView = CWStarRateView(frameAndImage: starsContainerView.frame, numberOfStars: 5, foreground: "review_star_yellow", background: "review_star_gray")
+            starRateView = StarRateView(frame: starsContainerView.frame)
+
             starRateView.userInteractionEnabled = true
             self.contentView.addSubview(starRateView)
             
             starRateView.snp_makeConstraints { (make) in
                 make.edges.equalTo(starsContainerView)
             }
+        } else {
+            starRateView.frame = starsContainerView.frame
+            starRateView.layoutIfNeeded()
+     //       starRateView.relayoutStars()
         }
+        
+        serviceIcon.layer.cornerRadius = serviceIcon.height / 2
+    }
+    
+    func setToHeadComment() {
+        
+        heightConstraint.constant = originIconHeight + 10
+        
+        starContainerHeightConstraint.constant = originStarContainerHeight + 10
+        starContainerWidthConstraint.constant = originStarContainerWidth + 30
+        
+        contentView.layoutIfNeeded()
+ 
+    }
+    
+    func setToSubComment() {
+        heightConstraint.constant = originIconHeight
+        
+        starContainerHeightConstraint.constant = originStarContainerHeight
+        starContainerWidthConstraint.constant = originStarContainerWidth
+        
+        contentView.layoutIfNeeded()
     }
     
 }
