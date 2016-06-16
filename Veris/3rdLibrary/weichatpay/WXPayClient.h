62b51ed53dd0dbada912c3eed5e7ef5a829d963d
tinkl <nicolastinkl@gmail.com>
2016-06-16 19:07:13 +0800

    # 添加微信支付和支付宝支付

---
+++ Veris/3rdLibrary/weichatpay/WXPayClient.h
@@ -0,0 +1,38 @@
+//
+//  WXPayClient.h
+//  WechatPayDemo
+//
+//  Created by Alvin on 3/22/14.
+//  Copyright (c) 2014 Alvin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "WXApi.h"
+#import "JSONModel.h"
+
+
+
+/*!
+ *  支付类型
+ order_type: 业务类型 1 => 充值, 2 => 快件超期支付, 3 => 租用, 4 => 租用超期, 5 => 存放, 6 => 存放超期, 7 => 寄件
+ for_id:业务ID
+ pay_type: 支付类型，1 => 余额， 2 => 支付宝， 3 => 微信
+ fee:金额  分为单位
+ */
+@interface MDPayTypeModel : JSONModel
+
+@property (nonatomic, assign) NSInteger             order_type;
+@property (nonatomic, strong) NSString<Optional>    *for_id;
+@property (nonatomic, assign) NSInteger             pay_type;
+@property (nonatomic, assign) double                fee;
+@property (nonatomic, strong) NSString<Optional>    *sign_str;
+
+@end
+
+@interface WXPayClient : NSObject
+
++ (instancetype)shareInstance;
+
+- (void)payProduct:(MDPayTypeModel * ) model withNotify:(NSString *) notify;
+
+@end
---
+++ Veris/Business/payment/ViewController/AIPaymentViewController.swift
@@ -0,0 +1,52 @@
+//
+//  AIWishVowViewController.swift
+//  AIVeris
+//
+// Copyright (c) 2016 ___ASIAINFO___
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+import Foundation
+import UIKit
+import Spring
+import Cartography
+import AIAlertView
+import SnapKit
+
+/// 许愿视图
+class AIPaymentViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let navi = AINavigationBar.initFromNib() as? AINavigationBar{
+            view.addSubview(navi)
+            navi.holderViewController = self
+            constrain(navi, block: { (layout) in
+                layout.left == layout.superview!.left
+                layout.top == layout.superview!.top
+                layout.right == layout.superview!.right
+                layout.height == 44.0 + 10.0
+            })
+            
+        }
+        
+    }    
+    
+}
