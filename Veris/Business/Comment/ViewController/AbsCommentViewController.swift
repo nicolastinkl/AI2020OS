30cc356eef2f344c76e8901e09bc4c7bcb8716d7
admin <liliang9@asiainfo.com>
2016-06-20 10:43:39 +0800

    修改符合评论界面

--- Veris/Business/Comment/ViewController/AbsCommentViewController.swift
+++ Veris/Business/Comment/ViewController/AbsCommentViewController.swift
@@ -20,17 +20,17 @@ extension AbsCommentViewController: UINavigationControllerDelegate {
 
 extension AbsCommentViewController: CommentDistrictDelegate {
     func pohotImageButtonClicked(button: UIImageView, buttonParent: UIView) {
-        let alert = UIAlertController(title: nil, message: "选择图片来源", preferredStyle: .ActionSheet)
+        let alert = UIAlertController(title: nil, message: "AbsCommentViewController.selectSrc".localized, preferredStyle: .ActionSheet)
         
-        let actionCamera = UIAlertAction(title: "相机", style: .Default) { (UIAlertAction) in
+        let actionCamera = UIAlertAction(title: "Camera".localized, style: .Default) { (UIAlertAction) in
             BuildInCameraUtils.startCameraControllerFromViewController(self, delegate: self)
         }
         
-        let actionPhotosAlbum = UIAlertAction(title: "相册", style: .Default) { (UIAlertAction) in
+        let actionPhotosAlbum = UIAlertAction(title: "Gallery".localized, style: .Default) { (UIAlertAction) in
             BuildInCameraUtils.startMediaBrowserFromViewController(self, delegate: self)
         }
         
-        let actionCancel = UIAlertAction(title: "取消", style: .Cancel, handler: nil)
+        let actionCancel = UIAlertAction(title: "AIAudioMessageView.close".localized, style: .Cancel, handler: nil)
         
         alert.addAction(actionCamera)
         alert.addAction(actionPhotosAlbum)
