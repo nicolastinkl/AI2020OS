d826b27486ed7315fdf09896ae87c7fd18fa7940
admin <liliang9@asiainfo.com>
2016-06-20 15:19:56 +0800

    添加自服务卡片视图

---
+++ Veris/Business/CustomerCenter/View/PurchasedService/SubServiceCardView.swift
@@ -0,0 +1,21 @@
+//
+//  SubServiceCardView.swift
+//  AIVeris
+//
+//  Created by Rocky on 16/6/20.
+//  Copyright © 2016年 ___ASIAINFO___. All rights reserved.
+//
+
+import UIKit
+
+class SubServiceCardView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
