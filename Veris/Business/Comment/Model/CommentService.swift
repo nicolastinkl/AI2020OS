1d644e403e1b92cdb79575353a8c01a6a9076130
admin <liliang9@asiainfo.com>
2016-07-11 11:36:22 +0800

    创建服务评论模型，编写接口

--- Veris/Business/Comment/Model/CommentService.swift
+++ Veris/Business/Comment/Model/CommentService.swift
@@ -10,6 +10,63 @@ import Foundation
 
 
 protocol CommentService {
-    func getProposalList(success: (responseData: ProposalOrderListModel) -> Void, fail: (errType: AINetError, errDes: String) -> Void)
+    func getSingleComment(serviceId: String, success: (responseData: SingleComment) -> Void, fail: (errType: AINetError, errDes: String) -> Void)
+    func getCompondComment(serviceId: String, success: (responseData: CompondComment) -> Void, fail: (errType: AINetError, errDes: String) -> Void)
 
+}
+
+class HttpCommentService: CommentService {
+    func getSingleComment(serviceId: String, success: (responseData: SingleComment) -> Void, fail: (errType: AINetError, errDes: String) -> Void) {
+        
+        let message = AIMessage()
+        let url = AIApplication.AIApplicationServerURL.singleComment.description
+        message.url = url
+        
+        let data = ["service_id" : serviceId]
+        message.body = BDKTools.createRequestBody(data)
+        
+        AINetEngine.defaultEngine().postMessage(message, success: { (response) -> Void in
+            
+            do {
+                guard let dic = response as? [NSObject : AnyObject] else {
+                    fail(errType: AINetError.Format, errDes: "SingleComment JSON Parse Error...")
+                    return
+                }
+                let model = try SingleComment(dictionary: dic)
+                success(responseData: model)
+            } catch {
+                fail(errType: AINetError.Format, errDes: "SingleComment JSON Parse Error...")
+            }
+            
+        }) { (error: AINetError, errorDes: String!) -> Void in
+            fail(errType: error, errDes: errorDes ?? "")
+        }
+    }
+    
+    func getCompondComment(serviceId: String, success: (responseData: CompondComment) -> Void, fail: (errType: AINetError, errDes: String) -> Void) {
+        
+        let message = AIMessage()
+        let url = AIApplication.AIApplicationServerURL.compondComment.description
+        message.url = url
+        
+        let data = ["service_id" : serviceId]
+        message.body = BDKTools.createRequestBody(data)
+        
+        AINetEngine.defaultEngine().postMessage(message, success: { (response) -> Void in
+            
+            do {
+                guard let dic = response as? [NSObject : AnyObject] else {
+                    fail(errType: AINetError.Format, errDes: "CompondComment JSON Parse Error...")
+                    return
+                }
+                let model = try CompondComment(dictionary: dic)
+                success(responseData: model)
+            } catch {
+                fail(errType: AINetError.Format, errDes: "CompondComment JSON Parse Error...")
+            }
+            
+        }) { (error: AINetError, errorDes: String!) -> Void in
+            fail(errType: error, errDes: errorDes ?? "")
+        }
+    }
 }
