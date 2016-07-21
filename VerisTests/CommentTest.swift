3c100d3803dc6611e00691e732ab741e3c833b06
Rocky <liliang9@asiainfo.com>
2016-07-21 20:54:45 +0800

    实现提交订单接口，添加单元测试

--- VerisTests/CommentTest.swift
+++ VerisTests/CommentTest.swift
@@ -8,7 +8,7 @@
 
 import XCTest
 
-class CommentTest: XCTestCase {
+class CommentTest: SBOSSTestCase {
     
     var service: CommentService!
     
@@ -20,27 +20,6 @@ class CommentTest: XCTestCase {
     
     override func tearDown() {
         super.tearDown()
-    }
-
-    func testGetSingleComment() {
-        let readyExpectation = expectationWithDescription("done")
-        
-        service.getSingleComment("1", success: { (responseData) in
-            
-            let re = responseData
-            print("getSingleComment success:\(re)")
-            XCTAssert(true)
-            readyExpectation.fulfill()
-        }) { (errType, errDes) in
-            
-            XCTAssert(false)
-            readyExpectation.fulfill()
-
-        }
-        
-        waitForExpectationsWithTimeout(5, handler: { error in
-            XCTAssertNil(error, "Error")
-        })
     }
     
     func testGetCompondComment() {
@@ -57,6 +36,48 @@ class CommentTest: XCTestCase {
             XCTAssert(false)
             readyExpectation.fulfill()
             
+        }
+        
+        waitForExpectationsWithTimeout(5, handler: { error in
+            XCTAssertNil(error, "Error")
+        })
+    }
+    
+    func testQueryCommentSpecification() {
+        let readyExpectation = expectationWithDescription("done")
+        
+        service.queryCommentSpecification({ (responseData) in
+            let re = responseData
+            print("queryCommentSpecification success:\(re)")
+            XCTAssert(true)
+            readyExpectation.fulfill()
+        }) { (errType, errDes) in
+            XCTAssert(false)
+            readyExpectation.fulfill()
+        }
+        
+        waitForExpectationsWithTimeout(5, handler: { error in
+            XCTAssertNil(error, "Error")
+        })
+    }
+    
+    func testSubmitComments() {
+        let readyExpectation = expectationWithDescription("done")
+        
+        let serviceComment = ServiceComment()
+        serviceComment.service_id = "1"
+        serviceComment.spec_id = "2623"
+        serviceComment.value = "9"
+        serviceComment.text = "Good."
+        
+        service.submitComments("1", userType: 1, commentList: [serviceComment], success: { (responseData) in
+                let re = responseData
+                print("queryCommentSpecification success:\(re)")
+                XCTAssert(true)
+                readyExpectation.fulfill()
+            }) { (errType, errDes) in
+                XCTAssert(false)
+                readyExpectation.fulfill()
         }
         
         waitForExpectationsWithTimeout(5, handler: { error in
