2d91d5388775751385da1221c874c7ee126c6b98
liuxian <liuxian@google.com>
2016-08-10 17:35:19 +0800

    #update

--- Veris/Business/WorkManage/View/AIWorkQualificationView.swift
+++ Veris/Business/WorkManage/View/AIWorkQualificationView.swift
@@ -10,12 +10,32 @@ import UIKit
 import iCarousel
 
 class AIWorkQualificationView: UIView {
-
     
     @IBOutlet weak var topTitleLabel: UILabel!
     @IBOutlet weak var switchButton: UIButton!
     @IBOutlet weak var uploadButton: UIButton!
     @IBOutlet weak var carousel: iCarousel!
     @IBOutlet weak var imageTitleLabel: UILabel!
+    
+    //MARK: -> overrides
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.initSelfFromXib()
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupViews()
+    }
+    
+    func setupViews(){
+        setRoundCorner(switchButton)
+    }
+    
+    private func setRoundCorner(button: UIButton) {
+        button.layer.borderColor = UIColor.whiteColor().CGColor
+        button.layer.borderWidth = 1
+        button.layer.cornerRadius = 5
+    }
 
 }
