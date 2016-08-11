81a5207e154d31b7aa82d38f2eeb329a52e1b9a0
zx <zx19880528@gmail.com>
2016-08-11 14:57:59 +0800

    fix build error

--- Veris/Business/WorkManage/View/AIWorkManageHeaderView.swift
+++ Veris/Business/WorkManage/View/AIWorkManageHeaderView.swift
@@ -10,12 +10,15 @@ import UIKit
 
 class AIWorkManageHeaderView: UIView {
 	
+	var closeWidth: CGFloat = 100
+	var openWidth: CGFloat = 202
+    
 	required init?(coder aDecoder: NSCoder) {
 		super.init(coder: aDecoder)
 		setup()
 	}
 	
-	var filterColors = [
+	private var filterColors = [
 		UIColor(hexString: "#7b3990", alpha: 0.7),
 		UIColor(hexString: "#1c789f", alpha: 0.7),
 		UIColor(hexString: "#619505", alpha: 0.7),
@@ -23,49 +26,80 @@ class AIWorkManageHeaderView: UIView {
 		UIColor(hexString: "#b32b1d", alpha: 0.7),
 	]
 	
-	var closeWidth: CGFloat = 100
-	var openWidth: CGFloat = 200
-	var index = 0 {
-		didSet {
+	private var index = 0
+	
+	private var cardViews: [AIWorkManageHeaderCardView] = []
+	
+	private func setup() {
+		backgroundColor = UIColor.clearColor()
+		for i in 0...4 {
+			let card = AIWorkManageHeaderCardView.initFromNib() as! AIWorkManageHeaderCardView
+			card.filterColor = filterColors[i]
+			let tap = UITapGestureRecognizer(target: self, action: #selector(AIWorkManageHeaderView.cardViewTapped(_:)))
+			card.addGestureRecognizer(tap)
+			card.tag = i
+			addSubview(card)
+			cardViews.append(card)
+		}
+	}
+	
+	func setIndex(index: Int, animated: Bool = false) {
+		self.index = index
+		if animated {
 			updateConstraints()
+			setNeedsLayout()
+			UIView.animateWithDuration(0.25, animations: {
+				self.layoutIfNeeded()
+			})
+		} else {
+			updateConstraints()
+			setNeedsLayout()
 			layoutIfNeeded()
 		}
 	}
 	
-	var cardViews: [AIWorkManageHeaderCardView] = []
-	
-	func setup() {
-		backgroundColor = UIColor.clearColor()
-		for i in 0...4 {
-			let card = AIWorkManageHeaderCardView.initFromNib() as! AIWorkManageHeaderCardView
-			card.color = filterColors[i]
-			card.tag = i
-			addSubview(card)
-			cardViews.append(card)
-			
-			card.snp_makeConstraints(closure: { (make) in
-				make.leading.equalTo(CGFloat(i) * closeWidth)
-				make.top.bottom.equalTo(self)
-				make.width.equalTo(openWidth)
-			})
-		}
+	func cardViewTapped(tap: UITapGestureRecognizer) {
+		let i = tap.view!.tag
+		setIndex(i, animated: true)
 	}
 	
 	override func updateConstraints() {
 		super.updateConstraints()
-        cardViews.forEach { (card) in
-            let i = card.tag
-            card.snp_updateConstraints(closure: { (make) in
-                make.
-            })
-        }
+		cardViews.forEach { (card) in
+			let i = card.tag
+			card.snp_updateConstraints(closure: { (make) in
+				make.width.equalTo(cardWidth(cardIndex: i))
+				make.left.equalTo(cardLeft(cardIndex: i))
+				make.top.bottom.equalTo(self)
+			})
+		}
+	}
+	
+	// MARK: - helper
+	func cardLeft(cardIndex i: Int) -> CGFloat {
+		if i <= index {
+			return CGFloat(i) * closeWidth
+		} else {
+			return openWidth + CGFloat(index) * closeWidth
+		}
+	}
+	
+	func cardWidth(cardIndex i: Int) -> CGFloat {
+		if i == index {
+			return openWidth
+		} else {
+			return closeWidth
+		}
 	}
 }
 
 class AIWorkManageHeaderCardView: UIView {
-	var color: UIColor? {
+    
+    @IBOutlet weak var filterView: UIView!
+    
+	var filterColor: UIColor? {
 		didSet {
-			
+			filterView?.backgroundColor = filterColor
 		}
 	}
 	var leftText: String? {
@@ -88,5 +122,4 @@ class AIWorkManageHeaderCardView: UIView {
 			
 		}
 	}
-	
 }
