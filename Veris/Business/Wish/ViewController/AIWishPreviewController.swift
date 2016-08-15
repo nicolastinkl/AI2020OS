c21cc8095e00089a6826ef7850123f2ef0e7f034
tinkl <nicolastinkl@gmail.com>
2016-08-15 18:06:23 +0800

    # update

--- Veris/Business/Wish/ViewController/AIWishPreviewController.swift
+++ Veris/Business/Wish/ViewController/AIWishPreviewController.swift
@@ -68,7 +68,7 @@ class AIWishPreviewController: UIViewController {
         
         let QuestionTitle1 = AIWishTitleIconView.initFromNib() as! AIWishTitleIconView
         QuestionTitle1.icon.image = UIImage(named: "AI_Wish_Make_comment")
-        QuestionTitle1.title.text = "See what they say about the wish"
+        QuestionTitle1.title.text = "See what they say about it"
         QuestionTitle1.title.font = AITools.myriadBoldWithSize(20)
         addNewSubView(QuestionTitle1, preView: DescriptionLabel)
         QuestionTitle1.setHeight(53)
