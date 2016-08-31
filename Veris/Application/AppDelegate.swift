c04c0d1da2d502a584ef2fd3dd134cf11680bae0
王坜 <wangli13@asiainfo.com>
2016-08-31 11:07:05 +0800

    #增加卖家订单列表逻辑 #完善登录逻辑

--- Veris/Application/AppDelegate.swift
+++ Veris/Application/AppDelegate.swift
@@ -90,7 +90,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, WXApiDelegate {
             }
         }, route: "cddpl://.*")
 
-        showLoginViewController()
+        showRootViewController()
         handleLoacalNotifications()
 		return true
 		
@@ -291,16 +291,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate, WXApiDelegate {
 
     func refreshReLoginAction() {
         AILocalStore.logout()
-        AIAlertView().showNotice("Oops！", subTitle: "登录超时,请重新登录~", closeButtonTitle: nil, duration: 2, colorStyle: 0x727375, colorTextButton: 0xFFFFFF)
+
         let loginRootViewController = createLoginRootViewController()
         self.window!.rootViewController = loginRootViewController
+        AIAlertView().showNotice("Oops！", subTitle: "登录超时,请重新登录~", closeButtonTitle: nil, duration: 2, colorStyle: 0x727375, colorTextButton: 0xFFFFFF)
     }
 
     func logOut() {
         AILocalStore.logout()
-        AIAlertView().showNotice("真的要退出吗？亲~", subTitle: "", closeButtonTitle: nil, duration: 2, colorStyle: 0x727375, colorTextButton: 0xFFFFFF)
+
         let loginRootViewController = createLoginRootViewController()
         self.window!.rootViewController = loginRootViewController
+        AIAlertView().showNotice("真的要退出吗？亲~", subTitle: "", closeButtonTitle: nil, duration: 2, colorStyle: 0x727375, colorTextButton: 0xFFFFFF)
     }
 
     func createLoginRootViewController() -> UINavigationController {
@@ -324,6 +326,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate, WXApiDelegate {
         self.window?.makeKeyAndVisible()
     }
 
+    func showMainViewController() {
+        let root = AIRootViewController()
+        let mainRootViewController = UINavigationController(rootViewController: root)
+        mainRootViewController.navigationBarHidden = true
+
+        self.window = MBFingerTipWindow(frame: UIScreen.mainScreen().bounds)
+        self.window?.rootViewController = mainRootViewController
+        self.window?.makeKeyAndVisible()
+    }
+
+    func showRootViewController() {
+
+        if AILocalStore.didUserLogIn() == true {
+            showLoginViewController()
+        } else {
+            showMainViewController()
+        }
+    }
 
     //MARK: 配置默认用户
 	func configDefaultUser () {
--- Veris/Business/Seller/AISellerViewController.m
+++ Veris/Business/Seller/AISellerViewController.m
@@ -517,10 +517,36 @@
     AIProposalServiceModel *serviceModel = [[AIProposalServiceModel alloc] init];
     serviceModel.service_id = model.service.service_id;
 
-    AIRequirementViewController *requirementVC = [UIStoryboard storyboardWithName:@"UIRrequirementStoryboard" bundle:nil].instantiateInitialViewController;
-    requirementVC.orderPreModel = model;
+    model.service_type = @"1";
+    if ([model.service_type  isEqualToString:@"0"]) { // 单一服务
+        [self showSingalServiceStatusViewControllerWithStatus:model.service_status];
+    }else if ([model.service_type  isEqualToString:@"1"]){ // 复合服务
+        AIRequirementViewController *requirementVC = [UIStoryboard storyboardWithName:@"UIRrequirementStoryboard" bundle:nil].instantiateInitialViewController;
+        requirementVC.orderPreModel = model;
 
-    [self.navigationController pushViewController:requirementVC animated:YES];
+        [self.navigationController pushViewController:requirementVC animated:YES];
+    }
+
+}
+
+- (void)showSingalServiceStatusViewControllerWithStatus:(NSString *)status {
+    NSInteger statusInt = status.integerValue;
+
+    UIViewController *nextViewController = nil;
+    switch (statusInt) {
+        case 0:
+
+            break;
+
+        case 1:
+
+            break;
+
+        default:
+            break;
+    }
+
+    [self.navigationController pushViewController:nextViewController animated:YES];
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
