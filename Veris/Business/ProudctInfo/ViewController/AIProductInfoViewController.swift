9ddc7f62b937cf646a6d98c698bfb50e095f55e6
tinkl <nicolastinkl@gmail.com>
2016-09-01 20:46:48 +0800

    # update

--- Veris/Business/ProudctInfo/ViewController/AIProductInfoViewController.swift
+++ Veris/Business/ProudctInfo/ViewController/AIProductInfoViewController.swift
@@ -425,7 +425,7 @@ class AIProductInfoViewController: UIViewController {
             commentView?.initSubviews()
             commentView?.setWidth(UIScreen.mainScreen().bounds.width)
             commentView?.fillDataWithModel(commentModel)
-            commentView?.setHeight((commentView?.getheight() ?? 0) + hcom - 30)
+            commentView?.setHeight((commentView?.getheight() ?? 0) + hcom - 20)
             commentView?.bgView.hidden = true
             // Add Normal Answer Button
             commentView?.addBottomWholeSSBorderLineLeftMapping(AIApplication.AIColor.AIVIEWLINEColor, leftMapping: 40 / 3)
