804afab6b3403ccde2d91697e010e43c1766ef0b
Rocky <liliang9@asiainfo.com>
2016-09-01 16:23:01 +0800

    添加查询服务步骤节点详情接口，实现服务节点详情界面逻辑

--- Veris/Developement/Requirement/ServiceExcute/Logic/ExcuteManager.swift
+++ Veris/Developement/Requirement/ServiceExcute/Logic/ExcuteManager.swift
@@ -19,19 +19,31 @@ enum ResultCode: Int {
     case success = 1
 }
 
+enum PermissionType: Int {
+    case unneed = 0
+    case needPermision = 1
+}
+
+enum ProcedureStatus: Int {
+    case noStart = 0
+    case excuting = 1
+    case complete = 2
+}
+
 protocol ExcuteManager {
     func submitServiceNodeResult(nodeId: Int, resultList: [NodeResultContent], success: (responseData: RequestCode) -> Void, fail: (errType: AINetError, errDes: String) -> Void)
+    func queryProcedureInstInfo(procedureId: Int, userId: Int, success: (responseData: Procedure) -> Void, fail: (errType: AINetError, errDes: String) -> Void)
 }
 
 
 class BDKExcuteManager: ExcuteManager {
-    func submitServiceNodeResult(nodeId: Int, resultList: [NodeResultContent], success: (responseData: RequestCode) -> Void, fail: (errType: AINetError, errDes: String) -> Void) {
+    func submitServiceNodeResult(procedureId: Int, resultList: [NodeResultContent], success: (responseData: RequestCode) -> Void, fail: (errType: AINetError, errDes: String) -> Void) {
         
         let message = AIMessage()
         let url = AIApplication.AIApplicationServerURL.submitServiceNodeResult.description
         message.url = url
         
-        let data: [String: AnyObject] = ["procedure_inst_id": nodeId, "note_list": NodeResultContent.arrayOfDictionariesFromModels(resultList)]
+        let data: [String: AnyObject] = ["procedure_inst_id": procedureId, "note_list": NodeResultContent.arrayOfDictionariesFromModels(resultList)]
         message.body = BDKTools.createRequestBody(data)
         
         AINetEngine.defaultEngine().postMessage(message, success: { (response) -> Void in
@@ -50,5 +62,34 @@ class BDKExcuteManager: ExcuteManager {
         }) { (error: AINetError, errorDes: String!) -> Void in
             fail(errType: error, errDes: errorDes ?? "")
         }
+    }
+    
+    func queryProcedureInstInfo(serviceId: Int, userId: Int, success: (responseData: Procedure) -> Void, fail: (errType: AINetError, errDes: String) -> Void) {
+        
+        
+        let message = AIMessage()
+        let url = AIApplication.AIApplicationServerURL.queryProcedureInstInfo.description
+        message.url = url
+        
+        let data: [String: AnyObject] = ["service_inst_id": serviceId, "user_id": userId]
+        message.body = BDKTools.createRequestBody(data)
+        
+        AINetEngine.defaultEngine().postMessage(message, success: { (response) -> Void in
+            
+            do {
+                guard let dic = response as? [NSObject : AnyObject] else {
+                    fail(errType: AINetError.Format, errDes: "queryProcedureInstInfo JSON Parse Error...")
+                    return
+                }
+                let model = try Procedure(dictionary: dic)
+                success(responseData: model)
+            } catch {
+                fail(errType: AINetError.Format, errDes: "queryProcedureInstInfo JSON Parse Error...")
+            }
+            
+        }) { (error: AINetError, errorDes: String!) -> Void in
+            fail(errType: error, errDes: errorDes ?? "")
+        }
+        
     }
 }
