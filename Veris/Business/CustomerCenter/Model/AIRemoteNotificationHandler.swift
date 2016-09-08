159aab5c8b304005ef92ab8cc917ee65d8f763f1
liuxian <liuxian@google.com>
2016-09-08 17:07:50 +0800

    #update 调试抢单界面

--- Veris/Business/CustomerCenter/Model/AIRemoteNotificationHandler.swift
+++ Veris/Business/CustomerCenter/Model/AIRemoteNotificationHandler.swift
@@ -141,8 +141,8 @@ struct AIRemoteNotificationParameters {
 
         if let value = paramDic[key] as? String {
             if value == AIRemoteNotificationParameters.GrabOrderType {
-                UIViewController.showAlertViewController()
-                //let paramDic: Dictionary = userinfo["paramList"] as! Dictionary<String, String>
+                UIViewController.showAlertViewController(paramDic)
+                
                 
             } else if value == AIRemoteNotificationParameters.AudioAssistantType {
                 // 语音协助的 接受
