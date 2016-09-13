4f01d5afb07c10438a2dc3001c4a22756abc533a
zx <zx19880528@gmail.com>
2016-09-13 17:01:41 +0800

    modified:   Veris/Application/AIAnalytics.swift

--- Veris/Application/AIAnalytics.swift
+++ Veris/Application/AIAnalytics.swift
@@ -30,17 +30,17 @@ public class AIAnalytics: NSObject {
      @param keyName 自定义关键事件的标签. 关键事件标签用于区分同名事件，但不参与统计运算结果.
      @param attributes 自定义事件的属性列表.
      */
-	public class func beginEvent(eventId: String!, primarykey keyName: String!, attributes: [NSObject: AnyObject]!) {
+	public class func beginEvent(eventId: String!, primarykey keyName: String? = nil, attributes: [NSObject: AnyObject]!) {
 		var att = attributes
 		att.addEntriesFromDictionary(commonParams())
-		AVAnalytics.beginEvent(eventId, primarykey: keyName, attributes: att)
+		AVAnalytics.beginEvent(eventId, primarykey: keyName ?? "", attributes: att)
 	}
 	
 	/** 自定义事件,时长统计， 记录事件结束。
      @param eventId 自定义事件的Id.
      @param keyName 自定义关键事件的标签. 关键事件标签用于区分同名事件，但不参与统计运算结果.
      */
-	public class func endEvent(eventId: String!, primarykey keyName: String!) {
-		AVAnalytics.endEvent(eventId, primarykey: keyName)
+	public class func endEvent(eventId: String!, primarykey keyName: String? = nil) {
+		AVAnalytics.endEvent(eventId, primarykey: keyName ?? "")
 	}
 }
