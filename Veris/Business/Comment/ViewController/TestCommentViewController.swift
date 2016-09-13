e1515ef7cb7d62c0469099d406abeeb0b29ffa3e
Rocky <liliang9@asiainfo.com>
2016-09-13 15:42:18 +0800

    修复符合评论界面错乱的bug

--- Veris/Business/Comment/ViewController/TestCommentViewController.swift
+++ Veris/Business/Comment/ViewController/TestCommentViewController.swift
@@ -22,7 +22,7 @@ class TestCommentViewController: UIViewController {
     }
     
     @IBAction func testAction(sender: AnyObject) {
-        let vc = TextAndAudioInputViewController.initFromNib()
+        let vc = CompondServiceCommentViewController.initFromNib()
         
         let nav = UINavigationController(rootViewController: vc)
         presentViewController(nav, animated: true, completion: nil)
