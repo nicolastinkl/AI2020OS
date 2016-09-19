b38de5cc59ab38a9158026c2535b975151b494c6
Rocky <liliang9@asiainfo.com>
2016-09-19 12:52:58 +0800

    修改图片大小

--- Veris/Business/Comment/View/ServiceCommentTableViewCell.swift
+++ Veris/Business/Comment/View/ServiceCommentTableViewCell.swift
@@ -11,7 +11,7 @@ import UIKit
 class ServiceCommentTableViewCell: UITableViewCell {
     
     private static let commentAreaMaxHeight: CGFloat = 242
-    private let checkboxOffColor = UIColor.blackColor()
+    private let checkboxOffColor = UIColor(hex: "464856")
     private let checkboxOnColor = UIColor(hex: "B6BBE6")
 
     @IBOutlet weak var imageButton: UIImageView!
