73ea23e27194e91cfba49043b18965239b170b9e
tinkl <nicolastinkl@gmail.com>
2016-11-01 20:22:13 +0800

    # 连贯我的代收和代付

--- Veris/Business/FundManage/AIFundManageViewController.swift
+++ Veris/Business/FundManage/AIFundManageViewController.swift
@@ -8,26 +8,31 @@
 
 import UIKit
 
+/// 我的钱包首页
 class AIFundManageViewController: AIBaseViewController {
 
     @IBOutlet weak var contentScrollView: UIScrollView!
     private var preCacheView: UIView = UIView()
+    @IBOutlet weak var zijinButton: UIButton!
+    
+    @IBOutlet weak var nianshouruView: UIView!
+    @IBOutlet weak var daishouView: UIView!
+    @IBOutlet weak var daifuView: UIView!
     
     private let contentArray = ["我的余额", "我的信用积分", "我的商家币", "我的优惠券", "我的资金账户", "我的会员卡"]
     private let contentArrayColor = ["#7b3990", "#1c789f", "#619505", "#f79a00", "#d05126", "#b32b1d"]
     
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         //makeBackButton()
         // Do any additional setup after loading the view.
-        self.title = "My Wattet"
+        
         setupNavigationBar()
         
         // add view controller to this vc
         //let vc = AIMyWalletBalanceViewController.initFromNib()
-        //let vc = AIWillPayVController.init()
-        //presentBlurViewController(vc, animated: true, completion: nil)        
+        
        
     }
     
@@ -43,10 +48,15 @@ class AIFundManageViewController: AIBaseViewController {
                 self.addNewSubView(cell)
                 index  = index + 1
                 cell.tag = index
-                let tap = UITapGestureRecognizer(target: self, action: Selector("showAction:"))
+                let tap = UITapGestureRecognizer(target: self, action: #selector(AIFundManageViewController.showAction(_:)))
                 cell.addGestureRecognizer(tap)
             }
         }
+        
+    }
+    
+    //资金点击触发
+    @IBAction func moneyListAction(sender: AnyObject) {
         
     }
     
@@ -72,6 +82,20 @@ class AIFundManageViewController: AIBaseViewController {
             case 6:
                 //我的会员卡
                 break
+            case 7:
+                //年收入
+                break
+            case 8:
+                // 待fu
+                let vc = AIWillPayVController.init()
+                presentBlurViewController(vc, animated: true, completion: nil)
+                
+                break
+            case 9:
+                // 待收
+                let vc = AIWillReceiverVController.init()
+                presentBlurViewController(vc, animated: true, completion: nil)
+                break
                 
             default:
                 break
@@ -94,6 +118,49 @@ class AIFundManageViewController: AIBaseViewController {
         let font = AITools.myriadSemiCondensedWithSize(72.displaySizeFrom1242DesignSize())
         navigatonBarAppearance!.titleOption = UINavigationBarAppearance.TitleOption(bottomPadding: 40.displaySizeFrom1242DesignSize(), font: font, textColor: UIColor.whiteColor(), text: "我的钱包")
         setNavigationBarAppearance(navigationBarAppearance: navigatonBarAppearance!)
+        
+        zijinButton.layer.cornerRadius = 4
+        zijinButton.layer.masksToBounds = true
+        
+        localCode {
+            let badge = GIBadgeView()
+            badge.badgeValue = 13
+            badge.topOffset = 10
+            badge.rightOffset = 5
+            badge.font = AITools.myriadLightSemiExtendedWithSize(12)
+            self.nianshouruView.addSubview(badge)
+            self.nianshouruView.tag = 7
+            let tap = UITapGestureRecognizer(target: self, action: #selector(AIFundManageViewController.showAction(_:)))
+            self.nianshouruView.addGestureRecognizer(tap)
+        }
+        
+        localCode {
+            let badge = GIBadgeView()
+            badge.badgeValue = 5
+            badge.topOffset = 10
+            badge.rightOffset = 5
+            badge.font = AITools.myriadLightSemiExtendedWithSize(12)
+            self.daifuView.addSubview(badge)
+            self.daifuView.tag = 8
+            let tap = UITapGestureRecognizer(target: self, action: #selector(AIFundManageViewController.showAction(_:)))
+            self.daifuView.addGestureRecognizer(tap)
+        }
+        
+        localCode {
+            let badge = GIBadgeView()
+            badge.badgeValue = 8
+            badge.topOffset = 10
+            badge.rightOffset = 5
+            badge.font = AITools.myriadLightSemiExtendedWithSize(12)
+            self.daishouView.addSubview(badge)
+            self.daishouView.tag = 9
+            let tap = UITapGestureRecognizer(target: self, action: #selector(AIFundManageViewController.showAction(_:)))
+            self.daishouView.addGestureRecognizer(tap)
+        }
+       
+        
+        
+       
         
         
     }
--- Veris/Business/MyWallet/WillReceiver/ViewController/AIWillReceiverVController.swift
+++ Veris/Business/MyWallet/WillReceiver/ViewController/AIWillReceiverVController.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import UIKit
+import Spring
 
 class AIWillReceiverVController: AIBaseViewController {
     
@@ -45,44 +46,104 @@ class AIWillReceiverVController: AIBaseViewController {
     }
     
     func initLayout() {
-        
         tableview.dataSource = self
         tableview.delegate = self
+        tableview.backgroundColor = UIColor.clearColor()
+        tableview.separatorColor = UIColor.clearColor()
+        
+        view.addSubview(tableview)
         
         let model1 = AIWillPayService.AIWillPayServiceModel()
         model1.saddress = "医院"
         model1.sname  = "海淀"
         model1.stime = 12
         model1.sprice = "¥23"
-        model1.simageurl = ""
+        model1.simageurl = "http://ofmrrc6zd.bkt.clouddn.com/%E5%AD%95%E6%A3%80%E6%97%A0%E5%BF%A7%E6%9C%8D%E5%8A%A1-%E5%9B%BE%E6%A0%87.png"
+        
         
         let model2 = AIWillPayService.AIWillPayServiceModel()
         model2.saddress = "服务器地址"
         model2.sname  = "服务"
         model2.stime = 123232
         model2.sprice = "¥800"
-        model2.simageurl = ""
+        model2.simageurl = "http://ofmrrc6zd.bkt.clouddn.com/%E6%98%A5%E9%9B%A8%E6%9C%8D%E5%8A%A1-%E5%9B%BE%E6%A0%87.png"
         
         dataSource.append(model1)
         dataSource.append(model2)
+        tableview.reloadData()
+
         
     }
 }
 
+
 extension AIWillReceiverVController: UITableViewDelegate, UITableViewDataSource {
+    
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return dataSource.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier("cell")
+        var contentView: AIWillPayVControllerCell?
+        if cell == nil {
+            cell = UITableViewCell(style: .Default, reuseIdentifier: "cell")
+            contentView = AIWillPayVControllerCell.initFromNib() as? AIWillPayVControllerCell
+            cell?.contentView.addSubview(contentView!)
+            contentView!.snp_makeConstraints { (make) in
+                make.edges.equalTo((cell?.contentView)!)
+            }
+//            提醒按钮
+            let buttonSS = DesignableButton(frame: CGRectMake(0, 0, 244/3, 96/3))
+            let buttonSurePay = DesignableButton(frame: CGRectMake(244/3+10, 0, 244/3, 96/3))
+            
+            buttonSS.cornerRadius = 5
+            buttonSS.borderColor = UIColor(hex: "0f86e8")
+            buttonSS.borderWidth = 3/2
+            buttonSS.backgroundColor = UIColor.clearColor()
+            buttonSS.setTitle("", forState: UIControlState.Normal)
+            buttonSS.setTitleColor(UIColor(hex: "0f86e8"), forState: UIControlState.Normal)
+            buttonSS.titleLabel?.font = UIFont.systemFontOfSize(15)
+            buttonSS.hidden = true
+            
+            buttonSurePay.cornerRadius = 5
+            buttonSurePay.titleLabel?.font = UIFont.systemFontOfSize(15)
+            buttonSurePay.borderColor = UIColor(hex: "ffffff")
+            buttonSurePay.borderWidth = 0.5
+            buttonSurePay.backgroundColor = UIColor.clearColor()
+            buttonSurePay.setTitle("提醒付款", forState: UIControlState.Normal)
+            buttonSurePay.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+            
+            contentView?.buttonView.addSubview(buttonSS)
+            contentView?.buttonView.addSubview(buttonSurePay)
+            
+            buttonSS.addTarget(self, action: #selector(AIWillPayVController.showAIRechargeView), forControlEvents: UIControlEvents.TouchUpInside)
+            
+        }
         
-//        var cell = AIWillPayVControllerCell.initFromNib() as? AIWillPayVControllerCell
-        //tableView.dequeueReusableCellWithIdentifier("cell")
-//        if (cell == nil ) {
-//            cell = AIWillPayVControllerCell.initFromNib() as? AIWillPayVControllerCell
-//        }
+        cell?.selectionStyle = .None
         
-        return   UITableViewCell()
+        cell?.backgroundColor = UIColor.clearColor()
+        let model = dataSource[indexPath.row]
+        contentView?.addresss.text = model.saddress ?? ""
+        contentView?.time.text = String(model.stime ?? 0)
+        contentView?.nameLabel.text = model.sname ?? ""
+        contentView?.priceLabel.text = model.sprice ?? ""
+        let url = NSURL(string: model.simageurl ?? "")!
+        contentView?.icon.sd_setImageWithURL(url)
+        
+        return cell!
+    }
+    
+    func showAIRechargeView() {
+        if let viewrech = AIRechargeView.initFromNib() as? AIRechargeView {
+            view.addSubview(viewrech)
+            viewrech.initSettings()
+            viewrech.snp_makeConstraints { (make) in
+                make.edges.equalTo(view)
+            }
+        }
+        
     }
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
@@ -90,8 +151,6 @@ extension AIWillReceiverVController: UITableViewDelegate, UITableViewDataSource 
     }
     
     func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        return 100
+        return 120
     }
-    
-    
 }
