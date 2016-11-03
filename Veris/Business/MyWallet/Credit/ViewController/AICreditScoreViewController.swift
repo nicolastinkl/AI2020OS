615c5f901097e42b505319e73dec31b02c7623c9
王坜 <wangli13@asiainfo.com>
2016-11-03 10:32:28 +0800

    fix

---
+++ Veris/Business/MyWallet/Credit/ViewController/AICreditScoreViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AICreditScoreViewController.swift
+//  AIVeris
+//
+//  Created by 王坜 on 16/11/2.
+//  Copyright © 2016年 ___ASIAINFO___. All rights reserved.
+//
+
+import UIKit
+
+class AICreditScoreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
