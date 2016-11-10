33e50b8251a58b2227d15629f35b040331b039da
tinkl <nicolastinkl@gmail.com>
2016-11-10 20:01:06 +0800

    # update 自由定制逻辑

--- Veris/Application/AppDelegate.swift
+++ Veris/Application/AppDelegate.swift
@@ -73,7 +73,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, WXApiDelegate {
 		
         //MARK: Share
         configUmengShare()
-    
+        
         // DeepLink
         router.registerBlock({ (deeplink) in
             
